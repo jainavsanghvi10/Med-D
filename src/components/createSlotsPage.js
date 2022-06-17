6b37f6562f63f60e5e867420db8a33d354ebb784
Anurag Singh Naruka <Anuragsinghnaruka@gmail.com>
2022-06-17 22:49:11 +0530

    Using mui components

--- src/components/createSlotsPage.js
+++ src/components/createSlotsPage.js
@@ -4,25 +4,45 @@ import { Link } from 'react-router-dom';
 import '../assets/styles/newDesign.css';
 
 import TextField from '@mui/material/TextField';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Stack from '@mui/material/Stack';
 
 export default function CreateSlotsPage() {
-    const [value, setValue] = useState(null);
     return (
-        <>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <DatePicker
-                    label="Basic example"
-                    value={value}
-                    onChange={(newValue) => {
-                        setValue(newValue);
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                />
-            </LocalizationProvider>
-        </>
+        <Stack component="form" noValidate spacing={3}>
+            <TextField
+                id="date"
+                label="Birthday"
+                type="date"
+                defaultValue="2017-05-24"
+                sx={{ width: 220 }}
+                InputLabelProps={{
+                shrink: true,
+                }}
+            />
+            <TextField
+                id="time"
+                label="Alarm clock"
+                type="time"
+                defaultValue="07:30"
+                InputLabelProps={{
+                shrink: true,
+                }}
+                inputProps={{
+                step: 300, // 5 min
+                }}
+                sx={{ width: 150 }}
+            />
+            <TextField
+                id="datetime-local"
+                label="Next appointment"
+                type="datetime-local"
+                defaultValue="2017-05-24T10:30"
+                sx={{ width: 250 }}
+                InputLabelProps={{
+                shrink: true,
+                }}
+            />
+        </Stack>
     );
 }
 
